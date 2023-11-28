9a68cd540d154b736599f2203c5b3a140e7aca2b
Amin192236 <77454893+Amin192236@users.noreply.github.com>
2023-11-28 23:59:16 +0330

    Update Orders_Create.py

--- Test/Orders_Create.py
+++ Test/Orders_Create.py
@@ -1,65 +1,90 @@
+
+import unittest
+import schedule
+from Locators import *
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
 from time import sleep
-
-from Locators import *
+from Pages.MongoDB.Mongo import SqlServer
+from Locators import code_admin
 from Pages.Login import LoginPage
-from Pages.Orders_Create import Orders_Create
-from Pages.Find_Element import FindElement
-import unittest
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+service = Service()
+options = webdriver.ChromeOptions()
+# options.add_argument("--headless")
+driver = webdriver.Chrome(service=service, options=options)
 
 
-class Test_Orders_Create(unittest.TestCase):
+class Test_Login(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.driver = driver
         cls.driver.maximize_window()
         cls.driver.implicitly_wait(5)
+        cls.tests_texts = []
 
-###Login
+    ###Loginssfsdfdsdsdaszsaa
 
-    def test01_login(self):
-        self.driver.get("http://testbpm.2ms.ir/login")
+    def test01_login_wrong_phone(self):
+        self.driver.get(base_url)
+        alert = Alert(driver=self.driver)
         login = LoginPage(driver=self.driver)
-        login.enter_login_username("9122367860")
+        login.enter_login_username("989122367800")
         login.enter_login_btn_submit_next()
         login.enter_login_password("43126")
         login.enter_login_btn_submit()
-        print("با موفقیت وارد پنل شد")
-###Orders_Create
+        sleep(2)
+        print(alert.text)
+        alert.accept()
+        message = "با زدن شماره اشتباه و کد صحیح وارد نمی شود."
+        self.tests_texts.append(message)
+        print(message)
 
-    def test02_orders_create(self):
-        orders_create = Orders_Create(driver=self.driver)
-        findel = FindElement(driver=self.driver)
-        orders_create.enter_click_orders_create()
-        findel.wait_until_element_has_an_attribute('xpath', orders_create_name_select, 'class', 'selection')
-        orders_create.enter_orders_create_name_select_text("ملیک")
-        sleep(1)
-        findel.wait_until_element_has_an_attribute('xpath', orders_create_name_select_option,
-                                                   'class', 'select2-results__option select2-results__option--highlighted')
-        # orders_create.enter_orders_create_name_select_option()
-        orders_create.enter_orders_create_check_transport()
-        orders_create.enter_orders_create_check_clearance()
-        orders_create.enter_orders_create_chooseservices()
-        print("با موفقیت تست بررسی شد.")
-
-###orders_create_products
-
-    def test03_orders_create(self):
-        orders_create = Orders_Create(driver=self.driver)
-        orders_create.enter_orders_create_products_holder()
-
-        sleep(3)
-
-        print("با موفقیت تست بررسی شد.")
-
+    # def test02_login_wrong_code(self):
+    #     self.driver.get(base_url)
+    #     alert = Alert(driver=self.driver)
+    #     login = LoginPage(driver=self.driver)
+    #     login.enter_login_username("989122367860")
+    #     login.enter_login_btn_submit_next()
+    #     login.enter_login_password("43136")
+    #     login.enter_login_btn_submit()
+    #     sleep(1)
+    #     print(alert.text)
+    #     alert.accept()
+    #     message = "با زدن شماره صحیح و کد اشتباه وارد نشد."
+    #     self.tests_texts.append(message)
+    #     print(message)
+    #
+    # def test03_login_not_phone(self):
+    #     self.driver.get(base_url)
+    #     alert = Alert(driver=self.driver)
+    #     login = LoginPage(driver=self.driver)
+    #     login.enter_login_btn_submit_next()
+    #     sleep(1)
+    #     print(alert.text)
+    #     alert.accept()
+    #     message = "بدون وارد کردن شماره تلفن وارد نشد."
+    #     self.tests_texts.append(message)
+    #     print(message)
+    #
+    # def test04_login(self):
+    #     self.driver.get(base_url)
+    #     login = LoginPage(driver=self.driver)
+    #     login.enter_login_username("989122367860")
+    #     login.enter_login_btn_submit_next()
+    #     login.enter_login_password(code_admin)
+    #     login.enter_login_btn_submit()
+    #     message = "با موفقیت وارد پنل شد و تست به درستی به پایان رسید."
+    #     self.tests_texts.append(message)
+    #     print(message)
 
     @classmethod
     def tearDownClass(cls) -> None:
-        sleep(5)
+        sleep(1)
         cls.driver.close()
         cls.driver.quit()
+
+
+SqlServer.run_tests_and_insert_into_sql_server(Test_Login, 'test_login')
